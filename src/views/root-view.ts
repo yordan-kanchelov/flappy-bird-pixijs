19b163d016e7942825017af94772fcede4b4aaed
Jordan Kanchelov <jkanchelov@gmail.com>
2018-09-02 16:48:48 +0300

    added fps counter

--- src/views/root-view.ts
+++ src/views/root-view.ts
@@ -1,4 +1,5 @@
 import { PixiConsole } from "pixi-console";
+import PixiFps from "pixi-fps";
 export class RootView extends PIXI.Container {
     private _stage: PIXI.Container;
 
@@ -6,8 +7,14 @@ export class RootView extends PIXI.Container {
         super();
         this.interactive = true;
         this._stage = stage;
+
         stage.addChild(this);
         stage.addChild(PixiConsole.getInstance());
+        stage.addChild(
+            new PixiFps({
+                fontSize: 12,
+            } as PIXI.TextStyle),
+        );
     }
 
     get stage(): PIXI.Container {
