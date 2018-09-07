b281a218e7b824a32c979e33b48488d4709e977d
jkanchelov <jkanchelov@gmail.com>
2018-09-07 17:30:12 +0300

    separating view from controller logic

--- src/controllers/obstacles-controller.ts
+++ src/controllers/obstacles-controller.ts
@@ -1,98 +1,97 @@
 import { PipeObstacle } from "../game-objects/pipes-obstacle";
 import { GameSettings } from "../models/game-settings";
 import { ObstaclesView } from "../views/obstacles-view";
+import { Ground } from "../game-objects/ground";
 
 export class ObstaclesController {
     private readonly PIPES_COUNT: number = 3;
-
+    
     private _view: ObstaclesView;
-    private _pipeObstacles: PipeObstacle[];
     private _gameSettings: GameSettings = GameSettings.getInstance();
-
+    
     private _nextPipeObstacleIndex: number;
-    private _moveObstacleTicker: PIXI.ticker.Ticker;
-
+    private _moveObstaclesTicker: PIXI.ticker.Ticker;
+    
     constructor(view: ObstaclesView) {
         this._view = view;
-        this._pipeObstacles = [];
-
+        this._view.on(ObstaclesView.PIPE_PASSED, this.onPipePassed, this)
+        
         this._nextPipeObstacleIndex = 0;
-
-        this._moveObstacleTicker = new PIXI.ticker.Ticker();
-        this._moveObstacleTicker.add(this.movePipes.bind(this));
-
+        
+        this._moveObstaclesTicker = new PIXI.ticker.Ticker();
+        this._moveObstaclesTicker.add(() => this.moveObstacles());
+        
         this.addObstacles();
     }
-
-    get PipeObstacles(): PipeObstacle[] {
-        return this._pipeObstacles;
-    }
+    
     get NextPipeObstacle(): PipeObstacle {
-        return this._pipeObstacles[this._nextPipeObstacleIndex];
+        return this._view.pipeObstacles[this._nextPipeObstacleIndex];
     }
 
-    public stopMoving(): void {
-        this._moveObstacleTicker.stop();
+    get GroundObstacle(): Ground {
+        return this._view.groundObstacle;
     }
-
+    
     public startMoving(): void {
-        this._moveObstacleTicker.start();
+        this._moveObstaclesTicker.start();
     }
-
+    
+    public stopMoving(): void {
+        this._moveObstaclesTicker.stop();
+    }
+    
     public resetObstacles(): void {
         this.resetPipesPosition();
     }
+    
+    private moveObstacles() {
+        this.movePipes();
+        this.moveGround();
+    }
+    // TODO:
+    // move this in the view
+    // refactor
+    // get event when the pipe is out of screen and update the obstacle afterward
+    private movePipes(): void {
+        this._view.movePipes();
+    }
+    
+    private moveGround(): void {
+        this._view.moveGround();
+    }
+    
+    private onPipePassed(): any {
+        this._nextPipeObstacleIndex = this._nextPipeObstacleIndex < this.PIPES_COUNT - 1 ? this._nextPipeObstacleIndex + 1 : 0; 
+        this._view.nextPipeObstacleIndex = this._nextPipeObstacleIndex;
+    }
 
     private addObstacles(): void {
+        this.addPipeObstacles();
+        this.addGroundObstacle();
+    }
+    
+    private addGroundObstacle() {
+        let ground = new Ground();
+        this._view.addGroundObstacle(new Ground(), this._gameSettings.gameHeight - ground.height)
+    }
+    
+    private addPipeObstacles() {
+        this._view.pipeObstacles = [];
+        
         for (let i = 0; i < 3; i++) {
             const pipeObstacle: PipeObstacle = new PipeObstacle();
             pipeObstacle.x =
-                this._gameSettings.gameWidth + pipeObstacle.width * i + i * this._gameSettings.obstaclesDistance;
-
+            this._gameSettings.gameWidth + pipeObstacle.width * i + i * this._gameSettings.obstaclesDistance;
             if (i == 0) pipeObstacle.isNextObstacle = true;
-
             this._view.addChild(pipeObstacle);
-            this._pipeObstacles.push(pipeObstacle);
+            this._view.pipeObstacles.push(pipeObstacle);
         }
     }
-
+    
     private resetPipesPosition(): void {
         this._nextPipeObstacleIndex = 0;
-        this._pipeObstacles[0].isNextObstacle = true;
-
-        // TODO:
-        // move this in the view;
-        for (let i = 0; i < this._pipeObstacles.length; i++) {
-            this._pipeObstacles[i].updateObstacle();
-            this._pipeObstacles[i].x =
-                this._gameSettings.gameWidth +
-                this._pipeObstacles[i].width * i +
-                i * this._gameSettings.obstaclesDistance;
-        }
-    }
-
-    // TODO:
-    // move this in the view
-    // refactor 
-    // get event when the pipe is out of screen and update the obstacle afterward
-    private movePipes(): void {
-        for (let i = 0; i < this._pipeObstacles.length; i += 1) {
-            if (
-                this._pipeObstacles[i].x <
-                this._gameSettings.birdStartingXPosition - PIXI.Texture.fromImage("birdMiddle.png").width / 2
-            ) {
-                if (this._nextPipeObstacleIndex < this.PIPES_COUNT - 1) this._nextPipeObstacleIndex++;
-                else {
-                    this._nextPipeObstacleIndex = 0;
-                }
-            }
-
-            if (this._pipeObstacles[i].x < -this._pipeObstacles[i].upperPipe.width) {
-                this._pipeObstacles[i].updateObstacle();
-                this._pipeObstacles[i].x = this._gameSettings.gameWidth + this._gameSettings.obstaclesDistance;
-            }
-
-            this._pipeObstacles[i].x -= this._gameSettings.obstaclesSpeed;
-        }
+        
+        this._view.resetPipesPossition();
+        this._view.pipeObstacles[0].isNextObstacle = true;
     }
 }
--- src/views/obstacles-view.ts
+++ src/views/obstacles-view.ts
@@ -1,5 +1,61 @@
+import { Ground } from "../game-objects/ground";
+import { PipeObstacle } from "../game-objects/pipes-obstacle";
+import { GameSettings } from "../models/game-settings";
+
 export class ObstaclesView extends PIXI.Container {
+    public static readonly PIPE_PASSED = "pipePassed";
+
+    groundObstacle: Ground;
+    pipeObstacles: PipeObstacle[];
+    nextPipeObstacleIndex: number = 0;
+
     constructor() {
         super();
+    }
+
+    addGroundObstacle(ground: Ground, yPos: number): any {
+        console.log(yPos);
+        this.groundObstacle = ground;
+        this.groundObstacle.y = yPos;
+        this.addChild(this.groundObstacle);
+    }
+
+    movePipes() {
+        if (
+            this.pipeObstacles[this.nextPipeObstacleIndex].x + this.pipeObstacles[this.nextPipeObstacleIndex].width <
+            this._gameSettings.birdStartingXPosition - PIXI.Texture.fromImage("birdMiddle.png").width / 2
+        ) {
+            this.emit(ObstaclesView.PIPE_PASSED);
+        }
+        for (let i = 0; i < this.pipeObstacles.length; i += 1) {
+            if (this.pipeObstacles[i].x < -this.pipeObstacles[i].upperPipe.width) {
+                this.pipeObstacles[i].updateObstacle();
+                this.pipeObstacles[i].x = this._gameSettings.gameWidth + this._gameSettings.obstaclesDistance;
+            }
+
+            this.pipeObstacles[i].x -= this._gameSettings.obstaclesSpeed;
+        }
+    }
+
+    moveGround() {
+        this.groundObstacle.x -= this._gameSettings.groundMoveSpeed;
+
+        if (-this.groundObstacle.x === this.groundObstacle.body.texture.width - this._gameSettings.gameWidth) {
+            this.groundObstacle.x = 0;
+        }
+    }
+
+    resetPipesPossition() {
+        for (let i = 0; i < this.pipeObstacles.length; i++) {
+            this.pipeObstacles[i].updateObstacle();
+            this.pipeObstacles[i].x =
+                this._gameSettings.gameWidth +
+                this.pipeObstacles[i].width * i +
+                i * this._gameSettings.obstaclesDistance;
+        }
+    }
+
+    private get _gameSettings(): GameSettings {
+        return GameSettings.getInstance();
     }
 }
