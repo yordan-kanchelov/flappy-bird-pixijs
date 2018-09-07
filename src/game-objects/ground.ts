3a0fcb021d4172c49c6d9f05e5e674bc783adecd
jkanchelov <jkanchelov@gmail.com>
2018-09-07 17:28:36 +0300

    removed move logic from the ground

--- src/game-objects/ground.ts
+++ src/game-objects/ground.ts
@@ -1,36 +1,13 @@
-import { GameSettings } from "../models/game-settings";
 import { GameObject } from "../interfaces/game-object";
 export class Ground extends PIXI.Sprite implements GameObject {
-    private ticker: PIXI.ticker.Ticker;
 
     constructor() {
         super();
 
         this.texture = PIXI.Texture.fromImage("ground.png");
-
-        this.ticker = new PIXI.ticker.Ticker();
-        this.ticker.add(this._startMoving, this);
     }
 
     get body(): PIXI.Sprite {
         return this;
-    }
-
-    // TODO: 
-    // move this logic in obstacle's controller
-    public startMoving() {
-        this.ticker.start();
-    }
-
-    public stopMoving() {
-        this.ticker.stop();
-    }
-
-    private _startMoving() {
-        this.x -= 2;
-
-        if (-this.x === this.body.texture.width - GameSettings.getInstance().gameWidth) {
-            this.x = 0;
-        }
     }
 }
