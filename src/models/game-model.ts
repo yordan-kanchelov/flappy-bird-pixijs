2af7d71b10d6349b720b6aaee2cb92e5e881ec47
Jordan Kanchelov <jkanchelov@gmail.com>
2018-11-24 22:47:11 +0200

    implementing game model

--- src/models/game-model.ts
+++ src/models/game-model.ts
@@ -1,3 +1,33 @@
 export class GameModel {
-    constructor() {}
+    public gameOver: boolean;
+
+    private _gameScore: number;
+
+    /**
+     *
+     */
+    constructor() {
+        this.initPropsWithDefaultValues();
+    }
+
+    public get gameScore(): number {
+        return this._gameScore;
+    }
+
+    incrementScore(incValue: number) {
+        this._gameScore += incValue;
+    }
+
+    decrementScore(decValue: number) {
+        this._gameScore -= decValue;
+    }
+
+    resetModel() {
+        this.initPropsWithDefaultValues();
+    }
+
+    private initPropsWithDefaultValues(): any {
+        this.gameOver = false;
+        this._gameScore = 0;
+    }
 }
