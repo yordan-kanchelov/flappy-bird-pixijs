86cdfe195a55756e320a29acb1f82d8eb53df029
jkanchelov <jkanchelov@gmail.com>
2018-12-06 14:53:54 +0200

    added fly sound

--- src/behaviors/birdBehaviors/bird-fly-flappy-behavior.ts
+++ src/behaviors/birdBehaviors/bird-fly-flappy-behavior.ts
@@ -1,19 +1,29 @@
 import { IFlyBehavior } from "../../interfaces/behaviors/fly-behavior";
 import { IGameObject } from "../../interfaces/game-object";
+import { Howl } from "howler";
 
 export class BirdFlyFlappyBehavior implements IFlyBehavior {
     private static readonly FLY_VELOCITY_Y: number = 2.5;
 
     public gameObject: IGameObject;
 
+    private flySound: Howl;
+
     constructor(gameObject: IGameObject) {
         this.gameObject = gameObject;
+
+        this.flySound = new Howl({
+            src: ["../../../assets/sounds/sfx_wing.wav"],
+        });
     }
 
     public fly(): void {
-        if (this.gameObject.health !== 0) {
-            this.gameObject.velocityY = -BirdFlyFlappyBehavior.FLY_VELOCITY_Y;
+        if (this.gameObject.health === 0) {
+            return ;
         }
+        
+        this.gameObject.velocityY = -BirdFlyFlappyBehavior.FLY_VELOCITY_Y;
+        this.flySound.play();
     }
 
     dispose(): void {
