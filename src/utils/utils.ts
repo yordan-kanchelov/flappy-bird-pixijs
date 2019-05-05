fd989097ee6c4e815d48e7c4ab63caebc59f8836
jkanchelov <jkanchelov@gmail.com>
2019-05-05 21:28:03 +0300

    Update sound assets path

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,7 +1,7 @@
 import { Howl } from "howler";
 
 function getHowlSound(soundName: string, props?: IHowlProperties): Howl {
-    let soundSource = `../../assets/sounds/${soundName}`;
+    let soundSource = `/assets/sounds/${soundName}`;
     let howlProps: IHowlProperties = { src: soundSource, ...props };
 
     return new Howl(howlProps);
