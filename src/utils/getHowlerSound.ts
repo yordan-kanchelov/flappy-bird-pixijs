4e7afb72d07717e4b309fa4061f80ce6657ed459
Yordan Kanchelov <yordan.kanchelov@gmail.com>
2020-02-12 23:13:41 +0200

    Fix sounds path location

--- src/utils/getHowlerSound.ts
+++ src/utils/getHowlerSound.ts
@@ -1,7 +1,7 @@
 import { Howl } from "howler";
 
 export default function getHowlSound(soundName: string, props?: IHowlProperties): Howl {
-    let soundSource = `/assets/sounds/${soundName}`;
+    let soundSource = `./sounds/${soundName}`;
     let howlProps: IHowlProperties = { src: soundSource, ...props };
 
     return new Howl(howlProps);
